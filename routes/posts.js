f87b941227eb03f547beafa241243c933f2136ce
Singh Faujdar <Mrityunjay.SinghFaujdar@ellucian.com>
2021-05-23 14:02:29 +0530

    Adding authenticaion check for Posting

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
+const passport = require("passport");
 
-router.post("/create-post", postController.createPost);
+router.post(
+    "/create-post",
+    passport.checkAuthentication,
+    postController.createPost
+);
 
 module.exports = router;
