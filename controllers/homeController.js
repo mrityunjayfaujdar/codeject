65d67e5d93bc1d56d31549452de249e6e64aed8a
Singh Faujdar <Mrityunjay.SinghFaujdar@ellucian.com>
2021-05-23 13:12:41 +0530

    Displaying Posts of User

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,23 @@
 const Post = require("../models/post");
 
 module.exports.home = function (req, res) {
-    //res.cookie("user_id", 11); // Setting the value of cookie from response
+    // res.cookie("user_id", 11); // Setting the value of cookie from response
 
-    Post.find({user: req.user._id}, (err, posts) => {
-        console.log(posts);
-        return res.render("home", {
-            title: "home",
-            posts,
+    // Post.find({user: req.user._id}, (err, posts) => {
+    //     console.log(posts);
+    //     return res.render("home", {
+    //         title: "home",
+    //         posts,
+    //     });
+    // });
+
+    //Populating the user details along with post
+    Post.find({})
+        .populate("user")
+        .exec(function (error, posts) {
+            return res.render("home", {
+                title: "home",
+                posts,
+            });
         });
-    });
 };
